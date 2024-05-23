be6ca2188d688c0ba6340891584cbd9b3b756ba5
dvrk-dvys <ja.harr91@gmail.com>
2024-05-23 12:27:00 +0200

    streamlit basewbot-8

--- src/weather_bot.py
+++ src/weather_bot.py
@@ -1,15 +1,30 @@
 import streamlit
 import python_weather
 import argparse
+import asyncio
+from emoji import emojize
 
+
+
+
+class WeatherBot:
+    def __int__(self, args):
+        self.weather_client = self.init_weather_client()
+        print()
+
+    def init_weather_client(self):
+        if 'weather_client' not in streamlit.session_state:
+            streamlit.session_state.weather_client = python_weather.Client(format=python_weather.METRIC)
+        return streamlit.session_state.weather_client
 
 def main():
+    streamlit.title("Initializing Weather Chat Bot :cloud_with_lightning::robot::cloud_with_lightning:")
     print()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+#    parser = argparse.ArgumentParser()
 
-    args = parser.parse_args()
-    parser.add_argument('-c', '--config', default='./config/config.yaml', help='config file')
+#    args = parser.parse_args()
+#    parser.add_argument('-c', '--config', default='./config/config.yaml', help='config file')
 
     main()
